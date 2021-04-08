85d9a55f343e4ab11ac68b2fa62a9e01e9c51052
William Moon <william-moon@lambdastudents.com>
2021-04-08 16:39:25 -0700

    finished, got too focused and didn't commit. SORRY

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,6 +4,10 @@
     https://api.github.com/users/<your name>
 */
 
+import axios from "axios";
+// axios.get("https://api.github.com/users/willmoon19");
+
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -17,6 +21,20 @@
     and append the returned markup to the DOM as a child of .cards
 */
 
+// const divCard = document.querySelector(".card");
+
+const divCard = document.querySelector(".cards");
+
+axios
+.get("https://api.github.com/users/willmoon19")
+.then((res) => {
+    const gitInfo = res.data;
+    const gitProfile = profile(gitInfo);
+    divCard.append(gitProfile);
+})
+.catch((error) => {
+  console.log(error);
+});
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -28,7 +46,24 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ["https://api.github.com/users/tetondan",
+  "https://api.github.com/users/dustinmyers",
+  "https://api.github.com/users/justsml",
+  "https://api.github.com/users/luishrd",
+  "https://api.github.com/users/bigknell"];
+
+  followersArray.forEach((item) => {
+    axios
+    .get(item)
+    .then((res) => {
+      const friend = profile(res.data);
+      divCard.append(friend);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  });
+
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -52,9 +87,48 @@ const followersArray = [];
 
 /*
   List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
+https://api.github.com/users/tetondan
+https://api.github.com/users/dustinmyers
+https://api.github.com/users/justsml
+https://api.github.com/users/luishrd
+https://api.github.com/users/bigknell
 */
+
+function profile (obj){
+const profCard = document.createElement("div");
+const profImg = document.createElement("img");
+const profInfo = document.createElement("div");
+const profH3 = document.createElement("h3");
+const profP1 = document.createElement("p");
+const profP2 = document.createElement("p");
+const profP3 = document.createElement("p");
+const profA = document.createElement("a");
+const profP4 = document.createElement("p");
+const profP5 = document.createElement("p");
+const profP6 = document.createElement("p");
+profCard.classList.add("card");
+profInfo.classList.add("card-info");
+profH3.classList.add("name");
+profP1.classList.add("username");
+profP3.textContent = ("Profile:");
+profImg.setAttribute("src", obj.avatar_url);
+profA.setAttribute("href", obj.url);
+profH3.textContent = (obj.name);
+profP1.textContent = (obj.login);
+profP2.textContent = (`Location: ${obj.location}`);
+profA.textContent = (obj.url);
+profP4.textContent = (`Followers: ${obj.followers}`);
+profP5.textContent = (`Following: ${obj.following}`);
+profP6.textContent = (`Bio: ${obj.bio}`);
+profCard.appendChild(profImg);
+profCard.appendChild(profInfo);
+profInfo.appendChild(profH3);
+profInfo.appendChild(profP1);
+profInfo.appendChild(profP2);
+profInfo.appendChild(profP3);
+profInfo.appendChild(profP4);
+profInfo.appendChild(profP5);
+profInfo.appendChild(profP6);
+profP3.appendChild(profA);
+return profCard;
+};
